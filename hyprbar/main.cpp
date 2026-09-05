3a3ac7066a7b3bee73838f20548a16245cf7a260
hitori-chan <hitori1403@gmail.com>
2026-09-05 09:01:37 +0700

    all: bump patch versions for the icon fixes

--- hyprbar/main.cpp
+++ hyprbar/main.cpp
@@ -365,7 +365,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
 
     damageBars();
 
-    return {"hyprbar", "the awesome wibar, drawn by the compositor", "hitori", "4.5.10"};
+    return {"hyprbar", "the awesome wibar, drawn by the compositor", "hitori", "4.5.11"};
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {
